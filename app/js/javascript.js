b6b690e30768633532b86be8e2299d2b1723386a
Kenzo Mendoza <kenzom954@gmail.com>
2016-10-03 12:02:44 -0700

    setup canvas and ajax

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -1,5 +1,28 @@
 $( document ).ready(function(){
-  const url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+  const w = 900;
+  const h = 800;
+  function render(base, data){
+    const margin = {
+      top: 90,
+      bottom: 90,
+      right: 90,
+      left: 90
+    }
+    const width = w - (margin.left + margin.right);
+    const height = h - (margin.top + margin.bottom);
+
+    const svg = d3.select("#canvas")
+                  .append("svg")
+                  .attr("id","chart")
+                  .attr("width", width)
+                  .attr("height", height)
+
+    const chart = d3.select("#chart")
+                    .classed("display", true)
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+  }
+  const url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json';
   $.ajax({
     type: "GET",
     dataType: "json",
@@ -9,7 +32,9 @@ $( document ).ready(function(){
     complete: () =>{
     },
     success: (data) =>{
-      render(data.data)
+      const baseTemperature = data.baseTemperature;
+      const dataAPI = data.monthlyVariance;
+      render(baseTemperature,dataAPI);
     },
     fail: () =>{
       console.log('failure!')
